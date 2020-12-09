6a84428e96e1b2a66101c530e4f62bd0489748d0
johnfromsmd <johnfromsmd@gmail.com>
2020-12-09 13:13:09 +0700

    push

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+const names = "John";
+
+function greet(){
+  return "Halo guys " + names
+}
+
+const element = <h1>{greet(names)}</h1>
+
+ReactDOM.render(element, document.getElementById('master'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
