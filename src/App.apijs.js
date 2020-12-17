3b1d2a0e12e6d3fb732c4e0d3b240745de382a85
johnfromsmd <johnfromsmd@gmail.com>
2020-12-17 13:05:36 +0700

    spa & react-router-dom

---
+++ src/App.apijs.js
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+ class App extends Component{
+   constructor(props){
+     super(props)
+     this.state = {
+       items: [],
+       isLoading: true
+     }
+   }
+
+   componentDidMount(){
+     fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => this.setState({ items: data, isLoading:false}))
+   }
+
+  render(){
+    const { items, isLoading } = this.state
+    
+    if(isLoading){
+      return <p>Loading h</p>
+    }
+  
+    return (    
+    <div>
+      <ul>
+        { items.map((item, index) => 
+        <li key={index}>{item.name}</li>)}
+      </ul>
+    </div>
+  );
+ }
+}
+
+export default App;
