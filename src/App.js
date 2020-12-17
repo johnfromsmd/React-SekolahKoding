0846868db2015c4cd022b43b568baf9b609df5f8
johnfromsmd <johnfromsmd@gmail.com>
2020-12-17 13:54:56 +0700

    hosting d github

--- src/App.js
+++ src/App.js
@@ -1,55 +1,33 @@
-import { Component } from 'react';
+import React,{ Component } from 'react';
 // import logo from './logo.svg';
 import {BrowserRouter, Route, Link, Router, Switch} from 'react-router-dom'
 import './App.css';
 
-function Home(){
-  return <h2>Halaman Home</h2>
-}
 
-function ListView(){
-  return (
-    <div>
-  <h2> Semua User</h2>
-  <ul>
-    <Link to="user/john">John</Link>
-    <div></div>
-    <Link to="user/doe">Doe</Link>
-  </ul>
-  </div>
-  )
-}
-
-function DetailView({match}){
-  return <h2> return Ini halaman {match.params.name}</h2>
-}
-
-function NoMatch(){
-  return <h2> Ini halaman Eroor</h2>
-}
 
  class App extends Component{
   render(){
-    return (    
-      <BrowserRouter>
-        <nav>
-          <li><Link to ="/"> Home </Link></li>
-          <li><Link to ="/users"> User </Link></li>
-
-        </nav>
-
-        <main>
-          <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/users" exact component={ListView} />
-          <Route path="/user/:name" exact component={DetailView} />
-
-          <Route component={NoMatch} />
-          </Switch>
-        </main>
-      </BrowserRouter>
+    return (
+      <table>
+        <tr>
+          <Column />
+        </tr>
+      </table>    
   );
  }
+}
+
+class Column extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+
+        <td>1</td>
+        <td>2</td>
+      
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/App.spa-react-router.js
+++ src/App.spa-react-router.js
@@ -1,55 +1,55 @@
-import { Component } from 'react';
-// import logo from './logo.svg';
-import {BrowserRouter, Route, Link, Router, Switch} from 'react-router-dom'
-import './App.css';
+// import { Component } from 'react';
+// // import logo from './logo.svg';
+// import {BrowserRouter, Route, Link, Router, Switch} from 'react-router-dom'
+// import './App.css';
 
-function Home(){
-  return <h2>Halaman Home</h2>
-}
+// function Home(){
+//   return <h2>Halaman Home</h2>
+// }
 
-function ListView(){
-  return (
-    <div>
-  <h2> Semua User</h2>
-  <ul>
-    <Link to="user/john">John</Link>
-    <div></div>
-    <Link to="user/doe">Doe</Link>
-  </ul>
-  </div>
-  )
-}
+// function ListView(){
+//   return (
+//     <div>
+//   <h2> Semua User</h2>
+//   <ul>
+//     <Link to="user/john">John</Link>
+//     <div></div>
+//     <Link to="user/doe">Doe</Link>
+//   </ul>
+//   </div>
+//   )
+// }
 
-function DetailView({match}){
-  return <h2> return Ini halaman {match.params.name}</h2>
-}
+// function DetailView({match}){
+//   return <h2> return Ini halaman {match.params.name}</h2>
+// }
 
-function NoMatch(){
-  return <h2> Ini halaman Eroor</h2>
-}
+// function NoMatch(){
+//   return <h2> Ini halaman Eroor</h2>
+// }
 
- class App extends Component{
-  render(){
-    return (    
-      <BrowserRouter>
-        <nav>
-          <li><Link to ="/"> Home </Link></li>
-          <li><Link to ="/users"> User </Link></li>
+//  class App extends Component{
+//   render(){
+//     return (    
+//       <BrowserRouter>
+//         <nav>
+//           <li><Link to ="/"> Home </Link></li>
+//           <li><Link to ="/users"> User </Link></li>
 
-        </nav>
+//         </nav>
 
-        <main>
-          <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/users" exact component={ListView} />
-          <Route path="/user/:name" exact component={DetailView} />
+//         <main>
+//           <Switch>
+//           <Route path="/" exact component={Home} />
+//           <Route path="/users" exact component={ListView} />
+//           <Route path="/user/:name" exact component={DetailView} />
 
-          <Route component={NoMatch} />
-          </Switch>
-        </main>
-      </BrowserRouter>
-  );
- }
-}
+//           <Route component={NoMatch} />
+//           </Switch>
+//         </main>
+//       </BrowserRouter>
+//   );
+//  }
+// }
 
-export default App;
+// export default App;
